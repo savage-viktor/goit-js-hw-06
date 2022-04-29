e42fa0a228ba720ba21fc233aa34d9a725862a2b
savage-viktor <viktor.dkn@gmail.com>
2022-04-29 20:00:07 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form")
 
+const formSubmit = (event) => {
+    event.preventDefault()
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget
+
+
+    if (!email.value || !password.value) {
+        return alert("Please fill in all the fields");
+    }
+
+    console.log({ email: email.value, password: password.value })
+    form.reset()
+}
+
+
+form.addEventListener("submit", formSubmit)
