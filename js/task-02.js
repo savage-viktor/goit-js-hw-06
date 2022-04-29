96fcfbb7dfe9c97a0b473d4ec42411b242ea296f
savage-viktor <viktor.dkn@gmail.com>
2022-04-29 14:30:34 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredients = document.querySelector("#ingredients")
+
+const liIngr = ingredients.map(ingredient => {
+  const newEl = document.createElement("li")
+  newEl.classList.add("item")
+  newEl.textContent = `${ingredient}`
+  return newEl
+})
+
+ulIngredients.append(...liIngr)
