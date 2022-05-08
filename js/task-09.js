2a1204e4556a4cfa3382a1825f25c66690efd129
savage-viktor <viktor.dkn@gmail.com>
2022-05-08 10:49:00 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function buttonClickFn() {
+  const newColor = getRandomHexColor()
+  bodyRef.style.backgroundColor = newColor
+  spanRef.textContent = newColor
+}
+
+const spanRef = document.querySelector("span")
+const bodyRef = document.querySelector("body")
+const buttonRef = document.querySelector("button")
+
+buttonRef.addEventListener("click", buttonClickFn)
